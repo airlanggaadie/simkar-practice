3fe1599ea608758f94276d2082851fb953ac1278
airlanggaadie <airlangga_adie@yahoo.com>
2020-02-26 15:42:33 +0700

    show count on dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,12 +2,20 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Education;
+use App\Employee;
+use App\Position;
+use App\Status;
 
 class DashboardController extends Controller
 {
     public function index()
     {
-        return view('pages.dashboard');
+        return view('pages.dashboard',[
+            'c_employee' => Employee::count(),
+            'c_education' => Education::count(),
+            'c_position' => Position::count(),
+            'c_status' => Status::count()
+        ]);
     }
 }
--- resources/views/pages/dashboard.blade.php
+++ resources/views/pages/dashboard.blade.php
@@ -1,6 +1,7 @@
-@extends('layouts.app')
+@extends('layouts.dashboard')
     
 @section('title','Halaman Dashboard')
+@section('active-dashboard','active')
 
 @section('content')
     <!-- Begin Page Content -->
@@ -17,72 +18,72 @@
             <!-- Earnings (Monthly) Card Example -->
             <div class="col-xl-3 col-md-6 mb-4">
                 <div class="card border-left-primary shadow h-100 py-2">
-                    <div class="card-body">
+                    <a href="{{ route('employee.index') }}" class="card-body" style="text-decoration:none">
                         <div class="row no-gutters align-items-center">
                             <div class="col mr-2">
-                                <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Paket Travel)</div>
-                                <div class="h5 mb-0 font-weight-bold text-gray-800">20</div>
+                                <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Employee</div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $c_employee }}</div>
                             </div>
                             <div class="col-auto">
-                                <i class="fas fa-hotel fa-2x text-gray-300"></i>
+                                <i class="fas fa-user fa-2x text-gray-300"></i>
                             </div>
                         </div>
-                    </div>
+                    </a>
                 </div>
             </div>
 
             <!-- Earnings (Monthly) Card Example -->
             <div class="col-xl-3 col-md-6 mb-4">
                 <div class="card border-left-success shadow h-100 py-2">
-                    <div class="card-body">
+                    <a href="{{ route('education.index') }}" class="card-body" style="text-decoration:none">
                         <div class="row no-gutters align-items-center">
                             <div class="col mr-2">
-                                <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Transaksi</div>
-                                <div class="h5 mb-0 font-weight-bold text-gray-800">12</div>
+                                <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Education</div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $c_education }}</div>
                             </div>
                             <div class="col-auto">
-                                <i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
+                                <i class="fas fa-spinner fa-2x text-gray-300"></i>
                             </div>
                         </div>
-                    </div>
+                    </a>
                 </div>
             </div>
 
             <!-- Earnings (Monthly) Card Example -->
             <div class="col-xl-3 col-md-6 mb-4">
                 <div class="card border-left-info shadow h-100 py-2">
-                    <div class="card-body">
-                    <div class="row no-gutters align-items-center">
-                        <div class="col mr-2">
-                            <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Pending</div>
+                    <a href="{{ route('position.index') }}" class="card-body" style="text-decoration:none">
+                        <div class="row no-gutters align-items-center">
+                            <div class="col mr-2">
+                                <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Position</div>
                                 <div class="row no-gutters align-items-center">
                                     <div class="col-auto">
-                                        <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">0</div>
+                                        <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">{{ $c_position }}</div>
                                     </div>            
                                 </div>
                             </div>
                             <div class="col-auto">
-                                <i class="fas fa-spinner fa-2x text-gray-300"></i>
+                                <i class="fas fa-hotel fa-2x text-gray-300"></i>
                             </div>
                         </div>
-                    </div>
+                    </a>
                 </div>
             </div>
 
             <!-- Pending Requests Card Example -->
             <div class="col-xl-3 col-md-6 mb-4">
                 <div class="card border-left-warning shadow h-100 py-2">
-                    <div class="card-body">
+                    <a href="{{ route('status.index') }}" class="card-body" style="text-decoration:none">
                         <div class="row no-gutters align-items-center">
                             <div class="col mr-2">
-                                <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">Sukses</div>
-                                <div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
+                                <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">Status</div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $c_status }}</div>
                             </div>
                             <div class="col-auto">
                                 <i class="fas fa-check fa-2x text-gray-300"></i>
                             </div>
                         </div>
-                    </div>
+                    </a>
                 </div>
             </div>
         </div>        
