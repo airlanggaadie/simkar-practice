a1974a2673f428c2c4aabc1fd2995adffdd46ae0
airlanggaadie <airlangga_adie@yahoo.com>
2020-02-26 15:00:16 +0700

    update route

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -1,18 +1,18 @@
 <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
     <!-- Sidebar - Brand -->
-    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{ route('index') }}">
+    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="{{ url('dashboard') }}">
         <div class="sidebar-brand-icon rotate-n-15">
         <i class="fas fa-laugh-wink"></i>
         </div>
-        <div class="sidebar-brand-text mx-3">Admin</div>
+        <div class="sidebar-brand-text mx-3">simkar</div>
     </a>
 
     <!-- Divider -->
     <hr class="sidebar-divider my-0">
 
     <!-- Nav Item - Dashboard -->
-    <li class="nav-item active">
-        <a class="nav-link" href="{{ route('index') }}">
+    <li class="nav-item @yield('active-dashboard')">
+        <a class="nav-link" href="{{ url('dashboard') }}">
         <i class="fas fa-fw fa-tachometer-alt"></i>
         <span>Dashboard</span></a>
     </li>
@@ -20,27 +20,27 @@
     <!-- Divider -->
     <hr class="sidebar-divider">
 
-    <li class="nav-item active">
+    <li class="nav-item @yield('active-employee')">
         <a class="nav-link" href="{{ route('employee.index') }}">
-        <i class="fas fa-fw fa-hotel"></i>
+        <i class="fas fa-fw fa-user"></i>
         <span>Employee</span></a>
     </li>
 
-    <li class="nav-item active">
+    <li class="nav-item @yield('active-education')">
         <a class="nav-link" href="{{ route('education.index') }}">
-        <i class="fas fa-fw fa-images"></i>
+        <i class="fas fa-fw fa-spinner"></i>
         <span>Education</span></a>
     </li>
 
-    <li class="nav-item active">
+    <li class="nav-item @yield('active-position')">
         <a class="nav-link" href="{{ route('position.index') }}">
-        <i class="fas fa-fw fa-dollar-sign"></i>
+        <i class="fas fa-fw fa-hotel"></i>
         <span>Position</span></a>
     </li>
 
-    <li class="nav-item active">
+    <li class="nav-item @yield('active-status')">
         <a class="nav-link" href="{{ route('status.index') }}">
-        <i class="fas fa-fw fa-dollar-sign"></i>
+        <i class="fas fa-fw fa-check"></i>
         <span>Status</span></a>
     </li>
     <!-- Divider -->
